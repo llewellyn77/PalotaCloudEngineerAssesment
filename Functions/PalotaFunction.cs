101de81d68463b6e0fabec44c0da7696dae45177
Llewellyn <llewellyn77777@gmail.com>
2022-04-21 20:45:51 +0200

    Finished iso3Code function

--- Functions/PalotaFunction.cs
+++ Functions/PalotaFunction.cs
@@ -12,6 +12,7 @@ using RESTCountries.Services;
 using RESTCountries.Models;
 using System.Linq;
 using Newtonsoft.Json;
+using System;
 
 namespace Palota.Assessment.Countries.Functions
 {
@@ -23,10 +24,11 @@ namespace Palota.Assessment.Countries.Functions
             ILogger logger)
         {
             //Gets all countries
-            var all = await RESTCountriesAPI.GetAllCountriesAsync(); //gets and returns all information about the country
-            List<string> countries = all.Select(c => c.Name).ToList();
-        
+             var all = await RESTCountriesAPI.GetAllCountriesAsync(); //gets and returns all information about the country
+             List<string> countries = all.Select(c => c.Name).ToList();
 
+            //Test
+            //var result = await RESTCountriesAPI.GetCountriesByContinentAsync("africa");
 
             // Get all country names...
 
@@ -34,34 +36,97 @@ namespace Palota.Assessment.Countries.Functions
 
             return new OkObjectResult(countries);
         }
+        //List all countries given search
+        [FunctionName("ListAllCountriesInSpecificContinent")]
+        public static async Task<IActionResult> ListAllCountriesInSpecificContinent(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "continents/{continentName}/countries/")] HttpRequest request,
+           ILogger logger)
+        {
+            //Gets all countries from specific search
+            string continentName = request.Query["continentName"];
 
-      //  [FunctionName("GetASingleCountryByIso3Code")]
-      //public static async Task<IActionResult> GetASingleCountryByIso3Code(
-      //      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "countries/{iso3Code}")] HttpRequest request,
-      //     ILogger logger, string iso3Code)
-      // {
-      //      var data = @"{
-      //           ""name"": "",
-      //           ""iso3Code"": ""ZAF"",
-      //           ""capital"": ""Pretoria"",
-      //           ""subregion"": ""Southern Africa"",
-      //           ""region"": ""Africa"",
-      //           ""population"": 59308690,
-      //       ""location"": {           
-      //           ""lattitude"": -29.0,
-      //           ""longitude"": 24.0
-      //                   },
-      //           ""demonym"": ""South African"",
-      //           ""nativeName"": ""South Africa"",
-      //           ""numericCode"": ""710"",
-      //           ""flag"": ""https://flagcdn.com/za.svg""
-      // }";
+            string requestBody = String.Empty;
+            using (StreamReader streamReader = new StreamReader(request.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            continentName = continentName ?? data?.nameOfContinent;
+            var result = await RESTCountriesAPI.GetCountriesByContinentAsync(continentName);
+            List<string> SearchContinentResults = result.Select(d=> d.Name).ToList();
 
-      //      // Get book by id from database..
+            //return continentName != null
+            //    ? (ActionResult)new OkObjectResult($"{result}")
+            //    : new BadRequestObjectResult("Please pass a name of the continent the query string or in the request body");
 
-      //      logger.LogInformation($"Get country  from database.");
 
-      //  //   return new OkObjectResult(countries);
-      //}
+            logger.LogInformation($"Search continent");
+            return new OkObjectResult(SearchContinentResults);
+        }
+
+        //List all countries given iso3Code search
+        [FunctionName("ListSingleCountryiso3Code")]
+        public static async Task<IActionResult> GetASingleCountryByIso3Code(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "countries/{iso3Code}")] HttpRequest request,
+           ILogger logger)
+        {
+            //Gets all countries from specific search
+            string iso3Code = request.Query["iso3Code"];
+
+            string requestBody = String.Empty;
+            using (StreamReader streamReader = new StreamReader(request.Body))
+            {
+                requestBody = await streamReader.ReadToEndAsync();
+            }
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            iso3Code = iso3Code ?? data?.nameOfContinent;
+            var result = await RESTCountriesAPI.GetCountriesByCodesAsync(iso3Code);
+            List<string> Searchiso3CodeResults = result.Select(d => d.Name).ToList();
+
+            //return continentName != null
+            //    ? (ActionResult)new OkObjectResult($"{result}")
+            //    : new BadRequestObjectResult("Please pass a name of the continent the query string or in the request body");
+
+
+            logger.LogInformation($"Search with iso3Code");
+            return new OkObjectResult(Searchiso3CodeResults);
+        }
+
+
+
+
+        // Get all country names...
+
+
+
+
+        //  [FunctionName("GetASingleCountryByIso3Code")]
+        //public static async Task<IActionResult> GetASingleCountryByIso3Code(
+        //      [HttpTrigger(AuthorizationLevel.Function, "get", Route = "countries/{iso3Code}")] HttpRequest request,
+        //     ILogger logger, string iso3Code)
+        // {
+        //      var data = @"{
+        //           ""name"": "",
+        //           ""iso3Code"": ""ZAF"",
+        //           ""capital"": ""Pretoria"",
+        //           ""subregion"": ""Southern Africa"",
+        //           ""region"": ""Africa"",
+        //           ""population"": 59308690,
+        //       ""location"": {           
+        //           ""lattitude"": -29.0,
+        //           ""longitude"": 24.0
+        //                   },
+        //           ""demonym"": ""South African"",
+        //           ""nativeName"": ""South Africa"",
+        //           ""numericCode"": ""710"",
+        //           ""flag"": ""https://flagcdn.com/za.svg""
+        // }";
+
+        //      // Get book by id from database..
+
+        //      logger.LogInformation($"Get country  from database.");
+
+        //  //   return new OkObjectResult(countries);
+        //}
     }
 }
